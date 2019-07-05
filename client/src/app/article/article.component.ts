dcee02b55e44fdb11f65e529ba5437884f55af08
IInfinity <I.INF@Outlook.com>
2019-07-06 01:43:44 +0800

    feat(client/article): article draft, mobile layout

--- client/src/app/article/article.component.ts
+++ client/src/app/article/article.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Article {
+  title: string;
+  author: string;
+  date: number | Date;
+  image?: string;
+  summary: string;
+}
+
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
@@ -7,8 +15,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ArticleComponent implements OnInit {
 
+  public articles: Article[] = [
+    { title: '测试标题', author: '夜寒苏', date: Date.now(), image: 'assets/background-light.png', summary: '这里是摘要。这里是摘要。这里是摘要。这里是摘要。这里是摘要。' },
+    { title: '测试标题', author: '夜寒苏', date: Date.now(), summary: '这里是摘要。这里是摘要。这里是摘要。这里是摘要。这里是摘要。' },
+    { title: '测试标题', author: '夜寒苏', date: Date.now(), image: 'assets/background-light.png', summary: '这里是摘要。这里是摘要。这里是摘要。这里是摘要。这里是摘要。' },
+    { title: '测试标题', author: '夜寒苏', date: Date.now(), image: 'assets/background-light.png', summary: '这里是摘要。这里是摘要。这里是摘要。这里是摘要。这里是摘要。' },
+    { title: '测试标题', author: '夜寒苏', date: Date.now(), summary: '这里是摘要。这里是摘要。这里是摘要。这里是摘要。这里是摘要。' },
+    { title: '测试标题', author: '夜寒苏', date: Date.now(), summary: '这里是摘要。这里是摘要。这里是摘要。这里是摘要。这里是摘要。' },
+    { title: '测试标题', author: '夜寒苏', date: Date.now(), summary: '这里是摘要。这里是摘要。这里是摘要。这里是摘要。这里是摘要。' }
+  ];
+
   constructor() { }
 
   ngOnInit() { }
+
+  trackByFn(index: number, item: Article) {
+    return item.date;
+  }
 
 }
