51bc8f6050010cd7d81311929d988a3991f9bfd6
IInfinity <I.INF@Outlook.com>
2019-07-05 16:22:40 +0800

    refactor(client/app): move content to home compnent

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,29 +1,12 @@
-import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 import { AppService } from './app.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
-  animations: [
-    trigger('title', [
-      state('light', style({ color: '#999' })),
-      state('dark', style({ color: '#000' })),
-      transition('light <=> dark', [animate('1500ms ease-in-out')])
-    ])
-  ]
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-
-  public animation = {
-    title: {
-      callback: () => this.animation.title.state = this.animation.title.state === 'light' ? 'dark' : 'light',
-      state: 'dark'
-    }
-  };
-
-  public desc = 'Angular client + Rester server';
 
   constructor(
     public app: AppService
