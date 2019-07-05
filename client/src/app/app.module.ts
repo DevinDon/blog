9b64f38cdc1c4568d4805933adf2cb54dfc33add
IInfinity <I.INF@Outlook.com>
2019-07-05 09:47:39 +0800

    chore(client/app): import all component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,15 +3,23 @@ import { RouteReuseStrategy } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { AppComponent } from './app.component';
 import { AppService } from './app.service';
+import { ArticleComponent } from './article/article.component';
+import { ImageComponent } from './image/image.component';
 import { MaterialModule } from './module/material.module';
 import { RouteReuseHandler, RoutingModule } from './module/routing.module';
 import { SharedModule } from './module/shared.module';
+import { SongComponent } from './song/song.component';
+import { VideoComponent } from './video/video.component';
 
 @NgModule({
   bootstrap: [AppComponent],
   declarations: [
     AppComponent,
-    AboutComponent
+    AboutComponent,
+    ImageComponent,
+    ArticleComponent,
+    SongComponent,
+    VideoComponent
   ],
   imports: [
     SharedModule,
