2b27efe27ce077c1d6c7666200504003f71ddf6f
IInfinity <I.INF@Outlook.com>
2019-07-05 15:35:19 +0800

    feat(client/nav): add nav component

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { AppService } from '../app.service';
+
+interface Route {
+  icon: string;
+  link: string;
+  text: string;
+}
 
 @Component({
   selector: 'app-nav',
@@ -7,7 +14,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 
-  constructor() { }
+  public routes: Route[] = [
+    { icon: 'library_books', link: '/article', text: '文字' },
+    { icon: 'photo_library', link: '/image', text: '影像' },
+    { icon: 'library_music', link: '/song', text: '声音' },
+    { icon: 'video_library', link: '/video', text: '视频' },
+    { icon: 'library_add', link: '/about', text: '关于' }
+  ];
+
+  constructor(
+    public app: AppService
+  ) { }
 
   ngOnInit() { }
 
