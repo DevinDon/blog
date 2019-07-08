0ea5d4388df5882b12f7f8f3f069fc5db5229fea
IInfinity <I.INF@Outlook.com>
2019-07-08 12:15:35 +0800

    feat(client/content): add category tag

--- client/src/app/content/content.component.ts
+++ client/src/app/content/content.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { Category, Content } from './content.model';
+import { Content } from './content.model';
 
 @Component({
   selector: 'app-content',
