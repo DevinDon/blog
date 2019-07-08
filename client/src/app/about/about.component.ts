db2d8c6eb5a7cdb0b71feb6816aeec4297b98406
IInfinity <I.INF@Outlook.com>
2019-07-08 09:39:17 +0800

    feat(client/about): about component draft

--- client/src/app/about/about.component.ts
+++ client/src/app/about/about.component.ts
@@ -1,11 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Event {
+  date: number | Date;
+  title: string;
+  content: string;
+}
+
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss']
 })
 export class AboutComponent implements OnInit {
+
+  public events: Event[] = [
+    { date: new Date('2017-01-02'), title: '标题了标题', content: '这里是内容这里是内容内容内容内容内容。' },
+    { date: new Date('2017-01-02'), title: '标题了标题', content: '这里是内容这里是内容内容内容内容内容。' },
+    { date: new Date('2017-01-02'), title: '标题了标题', content: '这里是内容这里是内容内容内容内容内容。' },
+    { date: new Date('2017-01-02'), title: '标题了标题', content: '这里是内容这里是内容内容内容内容内容。' },
+    { date: new Date('2017-01-02'), title: '标题了标题', content: '这里是内容这里是内容内容内容内容内容。' },
+  ];
+
+  public mottos: string[] = [
+    '从善如登，从恶如崩。',
+    '宠辱不惊，悲喜无鸣。',
+    '但行好事，莫问前程。'
+  ];
 
   constructor() { }
 
