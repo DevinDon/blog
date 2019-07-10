345be4dca2326d283c6d59ba6d6cf7b465922de7
IInfinity <I.INF@Outlook.com>
2019-07-10 10:43:06 +0800

    feat(client/home): add random motto

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,19 +1,39 @@
-import { Component, OnInit } from '@angular/core';
-import { AppService } from '../app.service';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription, timer } from 'rxjs';
+import { ApiService } from '../api.service';
+import { destory } from '../other/destory';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
 
   public motto = '大道虽简，知易行难。';
 
+  public mottos: string[] = [
+    '大道虽简，知易行难'
+  ];
+
+  private subscriptions: Subscription[] = [];
+
   constructor(
-    public app: AppService
+    public api: ApiService
   ) { }
 
-  ngOnInit() { }
+  ngOnInit() {
+    this.api.get('/motto/more/10').subscribe(mottos => this.mottos = mottos);
+    this.subscriptions.push(
+      timer(5000, 5000)
+        .subscribe(
+          () => this.motto = this.mottos[Math.floor(Math.random() * this.mottos.length)]
+        )
+    );
+  }
+
+  ngOnDestroy() {
+    destory(this.subscriptions);
+  }
 
 }
