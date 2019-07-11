89c89aed260db2ffd501e712e1f3ad25a2b2777f
IInfinity <I.INF@Outlook.com>
2019-07-11 14:26:51 +0800

    fix(client/category): fix category images

--- client/src/app/category/category.component.ts
+++ client/src/app/category/category.component.ts
@@ -14,11 +14,11 @@ interface Category {
 export class CategoryComponent implements OnInit {
 
   public categories: Category[] = [
-    { image: 'assets/bg-1.jpg', link: '/content/article', name: '文字' },
-    { image: 'assets/bg-2.jpg', link: '/content/image', name: '意境' },
-    { image: 'assets/bg-3.jpg', link: '/content/song', name: '音乐' },
-    { image: 'assets/bg-4.jpg', link: '/content/video', name: '影视' },
-    { image: 'assets/bg-5.jpg', link: '/content/question', name: '解惑' }
+    { image: 'assets/category/1.jpg', link: '/content/article', name: '文字' },
+    { image: 'assets/category/2.jpg', link: '/content/image', name: '意境' },
+    { image: 'assets/category/3.jpg', link: '/content/song', name: '音乐' },
+    { image: 'assets/category/4.jpg', link: '/content/video', name: '影视' },
+    { image: 'assets/category/5.jpg', link: '/content/question', name: '解惑' }
   ];
 
   constructor() { }
