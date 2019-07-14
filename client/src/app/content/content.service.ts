9a3b790c1aa8874dcdd398df7949fc2a6ae6d277
IInfinity <I.INF@Outlook.com>
2019-07-14 11:34:56 +0800

    feat(client/content): add memory cache

--- client/src/app/content/content.service.ts
+++ client/src/app/content/content.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { of } from 'rxjs';
+import { tap } from 'rxjs/operators';
 import { ApiService } from '../api.service';
 import { Content } from './content.model';
 
@@ -7,16 +9,24 @@ import { Content } from './content.model';
 })
 export class ContentService {
 
+  public cache: { [index: number]: Content } = {};
+
   constructor(
     public api: ApiService
   ) { }
 
   getOneByID(id: number) {
-    return this.api.get<Content>(`/content/${id}`);
+    return (this.cache[id] && of({ status: true, content: this.cache[id] })) || this.api.get<Content>(`/content/${id}`);
   }
 
   getMoreByRandom(total: number) {
-    return this.api.get<Content[]>(`/content/random/${total}`);
+    return this.api.get<Content[]>(`/content/random/${total}`)
+      .pipe(tap(result => result.status && result.content.forEach(content => this.cache[content.id] = content)));
+  }
+
+  getMoreByRecent(total: number) {
+    return this.api.get<Content[]>(`/content/recent/${total}`)
+      .pipe(tap(result => result.status && result.content.forEach(content => this.cache[content.id] = content)));
   }
 
   like(id: number) {
